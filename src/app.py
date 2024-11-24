aef7fa1da6f1d978981618fd41df1c101b8ce2e4
Manrrolo <manuel.sc@uc.cl>
2024-11-24 19:35:32 -0300

    Fix deploy

--- src/app.py
+++ src/app.py
@@ -9,6 +9,7 @@ import assets
 # Inicializar la app con los estilos personalizados y Bootstrap
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "Proyecto Final"
+server = app.server
 
 # Layout base
 app.layout = html.Div([
